9998f6ad084558f11a2a88dcd803c4b987e4a569
Nicholas Langford <n.langford@student.unsw.edu.au>
2024-08-01 12:47:42 +1000

    Add code for wk9 Q1 & 4

--- tut09/chmod_if_public_write.c
+++ tut09/chmod_if_public_write.c
@@ -19,5 +19,47 @@ int main(int argc, char *argv[]) {
 // chmod a file if publically-writeable
 
 void chmod_if_needed(char *pathname) {
-    // TODO
+    struct stat s;
+    int output = stat(pathname, &s);
+    if (output != 0) { // 0 = success, non-zero = failure (see man 2 stat)
+        perror(pathname);
+        exit(1);
+    }
+    // struct stat s should now be filled with data (see man 7 inode)
+
+    mode_t mode = s.st_mode;
+
+    // Mode: 32 bit integer. Last 9 bits of the mode represent permissions.
+    // Ie. 101 000 011 AT THE END is r-x --- -wx
+    // S_IWOTH is a hash define for write public flag, 00002 (octal) = 0b000 000 010
+    //      (see man 7 inode & stat.h)
+    // Recall: & used to extract 1 bits
+    //      (vs | used to set bits to 1, ~ used to flip all bits,
+    //      ^ used to flip particular bits, a & (~b) used to set bits to 0)
+    if (!(mode & S_IWOTH)) {
+        printf("%s is not publically writable\n", pathname);
+        return;
+    }
+    // EXAMPLE, only considering last 9 permission bits.
+    // SI_IWOTH =       0b000000010     (i.e., 00002, where leading 0 means octal)
+    // mode =           0b010101010     (This is currently publicly writeable)
+    // S_IWOTH & mode = 0b000000010     (THIS IS NOT ZERO, i.e. "truthy")
+
+    // However, if mode = 0b010101000   (This is not public writeable)
+    // S_IWOTH & mode = 0               ("falsy")
+
+    printf("removing public write from %s\n", pathname);
+
+    mode_t new_mode = mode & ~S_IWOTH; // Removing the write public flag
+    // EXAMPLE
+    // SI_IWOTH = 0b0000000010
+    // mode =     0b0010101010 (This is currently public writeable)
+    // ~S_IWOTH = 0b1111111101
+    // mode & ~S_IWOTH = 0b0010101000 (REMOVED PUBLIC WRITE FLAG.)
+
+    // Change the mode (set perms) & check if successful
+    if (chmod(pathname, new_mode) != 0) {
+        perror(pathname);
+        exit(EXIT_FAILURE);
+    }
 }
--- tut09/print_diary.c
+++ tut09/print_diary.c
@@ -1,4 +1,3 @@
-// Wk 9 Q1 - print_diary.c
 // print $HOME/.diary to stdout
 
 #include <stdio.h>
@@ -10,17 +9,38 @@ const char *basename = ".diary";
 int main(int argc, char *argv[]) {
     // Write a C program, print_diary.c, which prints the
     // contents of the file $HOME/.diary to stdout
-    //The lecture example getstatus.c shows how to get the
-    //value of an environment variable.
+    //  The lecture example getstatus.c shows how to get the
+    // value of an environment variable.
 
-    // TODO - get the value of the HOME environment variable
-    // char *home_pathname = ...
+    char *home_pathname = getenv("HOME");
 
-    // TODO - construct diary_pathname & calculate length
-    // "$HOME/.diary"
-    // home_pathname / basename
+    if (home_pathname == NULL) {
+        home_pathname = ".";
+    }
 
-    // TODO - print the contents of the file to stdout
+    // "$HOME/.diary", i.e. home_pathname / basename
+    // +1 for '/', +1 for null terminator '\0'
+    int diary_pathname_len = strlen(home_pathname) + strlen(basename) + 2;
+    char *diary_pathname = malloc(diary_pathname_len);
+
+    snprintf(diary_pathname, diary_pathname_len, "%s/%s", home_pathname, basename);
+
+    FILE *stream = fopen(diary_pathname, "r");
+    if (stream == NULL) {
+        perror(diary_pathname);
+        exit(EXIT_FAILURE);
+    }
+
+    // Could instead do letter = fgetc(stream) before the loop & again at the end, after putchar
+    int letter;
+    while ((letter = fgetc(stream)) != EOF) {
+        // printf("%c", letter);
+        // fputc(letter, stdout);
+        putchar(letter);
+    }
+
+    free(diary_pathname);
+    fclose(stream);
 
     return 0;
 }
